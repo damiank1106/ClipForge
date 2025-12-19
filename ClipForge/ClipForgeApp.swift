c4051e1e6eb172e97a7338dca52cabda1976bcc0
damiank1106 <damiank1106@yahoo.com>
2025-12-18 18:27:20 -0600

    Initial Commit

---
+++ ClipForge/ClipForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipForgeApp.swift
+//  ClipForge
+//
+//  Created by Damian Krakowski on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipForge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipForge
+//
+//  Created by Damian Krakowski on 12/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
